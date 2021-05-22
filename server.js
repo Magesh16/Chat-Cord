184ae541e28f4fd5f41cfb16b937c97e73dcbddb
Magesh <mageshsk16@gmail.com>
2021-05-22 13:49:21 +0530

    Add socket.io

---
+++ server.js
@@ -0,0 +1,29 @@
+const path  =  require("path");
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+
+
+const app =express();
+const server = http.createServer(app);
+const io =socketio(server);
+
+// SET STATIC FOLDER
+app.use(express.static(path.join(__dirname, "public")));
+
+//RUN WHEN CLIENT CONNECTS
+io.on("connection", socket=>{
+    console.log("New WS connection!!");
+
+    socket.emit('message','Welcome to ChatCord');
+
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    socket.on('disconnect', ()=>{
+        io.emit('message', 'A user had left the chat'); 
+    })
+});
+
+const PORT =3000 || process.env.PORT;
+
+server.listen(PORT,()=> console.log(`server runing in the port ${PORT}`));
